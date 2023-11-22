cb852572443d2e741e3cc31ec0fc6eb8d75b2b19
Muhammad Rafli Silehu <raflisilehu20@gmail.com>
2023-11-22 23:04:46 +0700

    style: customize the display

--- resources/views/watch.blade.php
+++ resources/views/watch.blade.php
@@ -12,26 +12,106 @@
     <div class="w-screen flex">
         <div class=" m-10" style="flex: 2;">
             <div class="video-wrapper flex justify-start items-center mb-5">
-                <iframe class="shadow-slate-700 shadow-2xl rounded-lg" height="500px" width="100%" src="https://www.youtube.com/embed/qW3pRQ5-hS0" allow="autoplay" allowFullScreen title="YouTube Video"></iframe>
+                <iframe class="shadow-slate-700 shadow-2xl rounded-lg" height="500px" width="100%" src="https://www.youtube.com/embed/LKP-vZvjbh8" allow="autoplay" allowFullScreen title="YouTube Video"></iframe>
             </div>
             <h1 class="text-3xl">Attack on Titan Season 2 - Opening | Shinzou wo Sasageyo!</h1>
         </div>
-        <div class="flex border border-gray-500 flex-col bg-[#101820] rounded-3xl shadow-lg mt-10 mb-10, me-5" style="width: 350px;">
-            <div class="h-28 rounded-t-3xl bg-slate-800 p-5 flex items-center justify-center">
+        <div class="flex border border-gray-500 flex-col bg-[#101820] rounded-2xl shadow-lg mt-10 mb-10, me-5" style="width: 350px; height: 85vh;">
+            <div class="h-28 rounded-t-2xl bg-slate-800 p-5 flex items-center justify-center">
                 <h1 class="text-lg font-semibold">List Opening Naruto</h1>
             </div>
-            <div class="flex">
-                <div class="relative pt-4 ps-4">
-                    <p class="absolute bottom-0 right-0 rounded-md bg-black text-white px-4 py-1 m-1" style="font-size: 10px;">10:30</p>
-                    <img class="rounded-md" src="https://akcdn.detik.net.id/visual/2022/02/05/attack-on-titan_169.png?w=650" alt="title" style="max-width: 150px; height: auto;">
-                </div>
+            <div class="h-full overflow-y-auto">
+                <div class="flex">
+                    <div class="relative pt-4 ps-4">
+                        <p class="absolute bottom-0 right-0 rounded-md bg-black text-white px-4 py-1 m-1" style="font-size: 8px;">10:30</p>
+                        <img class="rounded-md" src="https://akcdn.detik.net.id/visual/2022/02/05/attack-on-titan_169.png?w=650" alt="title" style="max-width: 120px; height: auto;">
+                    </div>
 
-                <div class="p-4">
-                    <h2 class="text-xs font-semibold line-clamp-2">
-                        Judul Video yang Panjangnya Lebih Dari Dua Baris Akan Dipotong dengan elipsis
-                    </h2>
-                </div>
+                    <div class="p-4">
+                        <h2 class="text-xs font-semibold line-clamp-2">
+                            Attack on Titan Season 2 - Opening | Shinzou wo Sasageyo!
+                        </h2>
+                    </div>
 
+                </div>
+                <div class="flex">
+                    <div class="relative pt-4 ps-4">
+                        <p class="absolute bottom-0 right-0 rounded-md bg-black text-white px-4 py-1 m-1" style="font-size: 8px;">10:30</p>
+                        <img class="rounded-md" src="https://akcdn.detik.net.id/visual/2022/02/05/attack-on-titan_169.png?w=650" alt="title" style="max-width: 120px; height: auto;">
+                    </div>
+
+                    <div class="p-4">
+                        <h2 class="text-xs font-semibold line-clamp-2">
+                            Attack on Titan Season 2 - Opening | Shinzou wo Sasageyo!
+                        </h2>
+                    </div>
+
+                </div>
+                <div class="flex">
+                    <div class="relative pt-4 ps-4">
+                        <p class="absolute bottom-0 right-0 rounded-md bg-black text-white px-4 py-1 m-1" style="font-size: 8px;">10:30</p>
+                        <img class="rounded-md" src="https://akcdn.detik.net.id/visual/2022/02/05/attack-on-titan_169.png?w=650" alt="title" style="max-width: 120px; height: auto;">
+                    </div>
+
+                    <div class="p-4">
+                        <h2 class="text-xs font-semibold line-clamp-2">
+                            Attack on Titan Season 2 - Opening | Shinzou wo Sasageyo!
+                        </h2>
+                    </div>
+
+                </div>
+                <div class="flex">
+                    <div class="relative pt-4 ps-4">
+                        <p class="absolute bottom-0 right-0 rounded-md bg-black text-white px-4 py-1 m-1" style="font-size: 8px;">10:30</p>
+                        <img class="rounded-md" src="https://akcdn.detik.net.id/visual/2022/02/05/attack-on-titan_169.png?w=650" alt="title" style="max-width: 120px; height: auto;">
+                    </div>
+
+                    <div class="p-4">
+                        <h2 class="text-xs font-semibold line-clamp-2">
+                            Attack on Titan Season 2 - Opening | Shinzou wo Sasageyo!
+                        </h2>
+                    </div>
+
+                </div>
+                <div class="flex">
+                    <div class="relative pt-4 ps-4">
+                        <p class="absolute bottom-0 right-0 rounded-md bg-black text-white px-4 py-1 m-1" style="font-size: 8px;">10:30</p>
+                        <img class="rounded-md" src="https://akcdn.detik.net.id/visual/2022/02/05/attack-on-titan_169.png?w=650" alt="title" style="max-width: 120px; height: auto;">
+                    </div>
+
+                    <div class="p-4">
+                        <h2 class="text-xs font-semibold line-clamp-2">
+                            Attack on Titan Season 2 - Opening | Shinzou wo Sasageyo!
+                        </h2>
+                    </div>
+
+                </div>
+                <div class="flex">
+                    <div class="relative pt-4 ps-4">
+                        <p class="absolute bottom-0 right-0 rounded-md bg-black text-white px-4 py-1 m-1" style="font-size: 8px;">10:30</p>
+                        <img class="rounded-md" src="https://akcdn.detik.net.id/visual/2022/02/05/attack-on-titan_169.png?w=650" alt="title" style="max-width: 120px; height: auto;">
+                    </div>
+
+                    <div class="p-4">
+                        <h2 class="text-xs font-semibold line-clamp-2">
+                            Attack on Titan Season 2 - Opening | Shinzou wo Sasageyo!
+                        </h2>
+                    </div>
+
+                </div>
+                <div class="flex">
+                    <div class="relative pt-4 ps-4">
+                        <p class="absolute bottom-0 right-0 rounded-md bg-black text-white px-4 py-1 m-1" style="font-size: 8px;">10:30</p>
+                        <img class="rounded-md" src="https://akcdn.detik.net.id/visual/2022/02/05/attack-on-titan_169.png?w=650" alt="title" style="max-width: 120px; height: auto;">
+                    </div>
+
+                    <div class="p-4">
+                        <h2 class="text-xs font-semibold line-clamp-2">
+                            Attack on Titan Season 2 - Opening | Shinzou wo Sasageyo!
+                        </h2>
+                    </div>
+
+                </div>
             </div>
 
         </div>
