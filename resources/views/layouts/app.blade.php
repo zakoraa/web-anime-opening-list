85eebb37cb1dcfe0a2f507406bcf6ab6478f46f0
Muhammad Rafli Silehu <raflisilehu20@gmail.com>
2023-12-22 17:45:51 +0700

    fix: web icon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,9 +6,9 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="csrf-token" content="{{ csrf_token() }}">
   <title>zanime</title>
+  <link rel="icon" href="{{ asset('storage/z_rounded.png') }}" type="image/x-icon">
+  <link rel="shortcut icon" href="{{ asset('storage/z_rounded.png') }}" type="image/x-icon">
   @vite('resources/css/app.css')
-  <link rel="icon" href="{{ asset('storage/zakora_rounded.png') }}" type="image/x-icon">
-  <link rel="shortcut icon" href="{{ asset('storage/zakora_rounded.png') }}" type="image/x-icon">
   <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
   <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
   <script src="https://cdn.jsdelivr.net/npm/swiper@11.0.5/swiper-bundle.min.js"></script>
@@ -40,7 +40,6 @@
     var alert = document.getElementById('myAlert');
     alert.style.display = 'none';
   }
-
 </script>
 <script src="https://unpkg.com/@dotlottie/player-component@latest/dist/dotlottie-player.mjs" type="module"></script>
 
